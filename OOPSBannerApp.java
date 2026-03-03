bdb7ac49f75fd17792f70c46e724bd94101f6a63
ch7653-lab <ch7653@srmist.edu.in>
2026-03-03 09:41:40 +0530

    use case 1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ *
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Charan Tej
+ * @version 1.0
+ */
+class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
